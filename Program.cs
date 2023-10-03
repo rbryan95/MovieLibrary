0a8562e2eabc3b13f2add18ed8faef2b286225eb
rbryan95 <berd1495@gmail.com>
2023-10-03 14:25:12 -0500

    Input user choice - Display choices to user - Input user choice

--- Program.cs
+++ Program.cs
@@ -28,4 +28,17 @@ Console.WriteLine(movie.Display());
 
 MovieFile movieFile = new MovieFile(movieFilePath);
 
+string choice = "";
+do
+{
+  // display choices to user
+  Console.WriteLine("1) Add Movie");
+  Console.WriteLine("2) Display All Movies");
+  Console.WriteLine("Enter to quit");
+  // input selection
+  choice = Console.ReadLine();
+  logger.Info("User choice: {Choice}", choice);
+} while (choice == "1" || choice == "2");
+
+
 logger.Info("Program ended");
