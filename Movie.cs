5b84ad7d84e601904d62278d62e36847dc1408d4
rbryan95 <berd1495@gmail.com>
2023-10-03 14:57:03 -0500

    Add quotes around title if it contains a comma

--- Movie.cs
+++ Movie.cs
@@ -1,9 +1,28 @@
 public class Movie
 {
   public UInt64 movieId { get; set; }
-  public string title { get; set; }
+  // private field
+  string _title;
+  public string title
+  {
+    get
+    {
+      return this._title;
+    }
+    set
+    {
+      // if there is a comma(,) in the title, wrap it in quotes
+      this._title = value.IndexOf(',') != -1 ? $"\"{value}\"" : value;
+    }
+  }
   public List<string> genres { get; set; }
-  
+
+  // constructor
+  public Movie()
+  {
+    genres = new List<string>();
+  }
+
   public string Display()
     {
       return $"Id: {movieId}\nTitle: {title}\nGenres: {string.Join(", ", genres)}\n";
